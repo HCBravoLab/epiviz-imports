4463f59a30d01c4d208188b0908a80e58e9b7050
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2017-02-23 14:02:24 -0500

    export to global window namespace

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 'use strict';
 
 var jquery = require('./src/js/lib/jquery/jquery-1.8.2.js');
-var d3 = require('./src/js/lib/d3/d3.v3.js');
+var d3 = require('d3');
 var sprintf = require('sprintf');
 var epiviz = require('epiviz');
 
 module.exports = {
     sprintf: sprintf,
-    d3: d3,
     epiviz : epiviz
 };
-// console.log(sprintf);
-// window.d3 = d3;
-// window.sprintf = sprintf;
-// module.exports = epiviz;
+
+window.sprintf = sprintf;
+window.epiviz = epiviz;
