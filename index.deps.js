1b2b3a09380ec586ab299938f5c29505f055c465
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2018-01-14 23:31:50 -0500

    minor fixes

--- index.deps.js
+++ index.deps.js
@@ -3,7 +3,7 @@
 var jquery = require('./src/js/lib/jquery/jquery-1.8.2.js');
 var d3 = require('d3');
 var sprintf = require('sprintf');
-var epiviz = require('epiviz');
+var epiviz = require('./node_modules/epiviz/index.min.js');
 
 module.exports = {
     sprintf: sprintf,
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var d3 = require('d3');
 var sprintf = require('sprintf');
-var epiviz = require('epiviz');
+var epiviz = require('./node_modules/epiviz/index.min.js');
 
 module.exports = {
     sprintf: sprintf,
